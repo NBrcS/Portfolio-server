e5070b379e6573672098ef35baa155a4cf193002
Gabriel Nóbrega <carlosgabrielncs@gmail.com>
2022-09-21 13:56:42 -0300

    first files

---
+++ Server/src/test/java/dev/nbrcs/Portifolio/PortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nbrcs.Portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
